117cb64c3484841dd34f1abb8d537cb45682ae8a
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-04-14 16:14:45 +0600

    Написать API для выполнение афифмитических операций
    - Добавить пути в urls.py для представлений которые будут
    выполнять арифмитические операций (add, subtract, multiply, divide)
    - Написать логику для представлений

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+import json
 
-# Create your views here.
+
+def is_valid_types(a, b):
+    return (isinstance(a, int) and isinstance(b, int)) or (isinstance(a, float) and isinstance(b, float))
+
+
+def add_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        response = dict()
+        if request.body:
+            a, b = json.loads(request.body).values()
+            if is_valid_types(a, b):
+                response['answer'] = a + b
+            else:
+                return JsonResponse({
+                    'error': 'Types other than int and float are not allowed!'
+                })
+
+        return JsonResponse(response)
+
+    return HttpResponseNotAllowed('Only GET request is allowed!')
+
+
+def subtract_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        response = dict()
+        if request.body:
+            a, b = json.loads(request.body).values()
+            if is_valid_types(a, b):
+                response['answer'] = a - b
+            else:
+                return JsonResponse({
+                    'error': 'Types other than int and float are not allowed!'
+                })
+
+        return JsonResponse(response)
+
+    return HttpResponseNotAllowed('Only GET request is allowed!')
+
+
+def multiply_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        response = dict()
+        if request.body:
+            a, b = json.loads(request.body).values()
+            if is_valid_types(a, b):
+                response['answer'] = a * b
+            else:
+                return JsonResponse({
+                    'error': 'Types other than int and float are not allowed!'
+                })
+
+        return JsonResponse(response)
+
+    return HttpResponseNotAllowed('Only GET request is allowed!')
+
+
+def divide_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        response = dict()
+        if request.body:
+            a, b = json.loads(request.body).values()
+            if is_valid_types(a, b):
+                try:
+                    response['answer'] = a / b
+                except ZeroDivisionError:
+                    return JsonResponse({
+                        'error': 'Division by zero!'
+                    })
+            else:
+                return JsonResponse({
+                    'error': 'Types other than int and float are not allowed!'
+                })
+
+        return JsonResponse(response)
+
+    return HttpResponseNotAllowed('Only GET request is allowed!')
